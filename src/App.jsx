453990630be9eaea15c1fc08f8e981ad50a4a5db
Carolina <carolina.tiferes@globant.com>
2025-03-17 12:44:20 -0300

    game over, winner and rematch

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,75 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
+import { WINNING_COMBINATIONS } from "./assets/winning-combinations";
+
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
-  let currentPlayer = "X";
+	let currentPlayer = "X";
 	if (gameTurns.length > 0 && gameTurns[0].player === "X") {
 		currentPlayer = "O";
 	}
-  return currentPlayer;
+	return currentPlayer;
 }
 
 function App() {
-  const [gameTurns, setGameTurns] = useState([]);
+	const [gameTurns, setGameTurns] = useState([]);
 	//const [activePlayer, setActivePlayer] = useState("X");
-  const activePlayer = deriveActivePlayer(gameTurns);
+	const activePlayer = deriveActivePlayer(gameTurns);
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    //setActivePlayer(currentActive => currentActive === 'X' ? 'O' : 'X')
-    setGameTurns(prevTurns => { // prevTurns has THE LATEST information stored
-      let currentPlayer = deriveActivePlayer(prevTurns);
-      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns];
-      return updatedTurns;
-    })
+	let gameBoard = [...initialGameBoard.map(array => [...array])];
+
+	for (const turn of gameTurns) {
+		const { square, player } = turn;
+		const { row, col } = square;
+
+		gameBoard[row][col] = player;
+	}
+
+  let winner;
+
+	for (const combination of WINNING_COMBINATIONS) {
+		const firstSquareSymbol =
+			gameBoard[combination[0].row][combination[0].column];
+		const secondSquareSymbol =
+			gameBoard[combination[1].row][combination[1].column];
+		const thirdSquareSymbol =
+			gameBoard[combination[2].row][combination[2].column];
+
+		if (
+			firstSquareSymbol &&
+			firstSquareSymbol === secondSquareSymbol &&
+			firstSquareSymbol === thirdSquareSymbol
+		) {
+      winner = firstSquareSymbol;
+		}
+	}
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+	function handleSelectSquare(rowIndex, colIndex) {
+		//setActivePlayer(currentActive => currentActive === 'X' ? 'O' : 'X')
+		setGameTurns((prevTurns) => {
+			// prevTurns has THE LATEST information stored
+			let currentPlayer = deriveActivePlayer(prevTurns);
+			const updatedTurns = [
+				{ square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+				...prevTurns,
+			];
+			return updatedTurns;
+		});
+	}
+
+  function handleRestart() {
+    setGameTurns([]);
   }
+
 	return (
 		<main>
 			<div id="game-container">
@@ -41,11 +88,12 @@ function App() {
 						isActive={activePlayer === "O"}
 					/>
 				</ol>
-				<GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRestart} />}
+				<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
 				{/* "lifting state up" = do an action in this component with the trigger in the child component */}
 			</div>
 
-      <Log turns={gameTurns}/>
+			<Log turns={gameTurns} />
 		</main>
 	);
 }
